acd4e749b4c078f8d16bb0a041d6a00846f21cab
eswise <eswise@gmail.com>
2023-05-29 16:31:27 -0400

    Temperature Converter Solution Code

---
+++ ClassFundamentals/Exercises/TemperatureConverter/Program.cs
@@ -0,0 +1,8 @@
+﻿using TemperatureConverter;
+
+Temperature t1 = new Temperature();
+
+Console.Write("Enter temperature (K): ");
+t1.Kelvin = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"The temperature is {t1.Kelvin}K, {t1.Celsius}C, {t1.Fahrenheit}F.");
---
+++ ClassFundamentals/Exercises/TemperatureConverter/Temperature.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    public class Temperature
+    {
+        private double _kelvin;
+        public double Kelvin
+        {
+            get
+            {
+                return _kelvin;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _kelvin = 0;
+                }
+                else
+                {
+                    _kelvin = value;
+                }
+            }
+        }
+
+        public double Celsius
+        {
+            get { return _kelvin - 273.15; }
+        }
+
+        public double Fahrenheit
+        {
+            get { return (_kelvin - 273.15) * 9 / 5 + 32; }
+        }
+    }
+}
